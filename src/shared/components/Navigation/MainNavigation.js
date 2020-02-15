00be41a72d78b8ac3af28f5052d29b36ce4e65a3
phamus <sphamus@gmail.com>
2020-02-15 19:46:36 +0100

    side drawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
+import NavLink from "./NavLink";
+import SideDrawer from "./SideDrawer";
+import BackDrop from "../UIElement/Backdrop";
 
 const MainNavigation = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
+    <Fragment>
+      {drawerIsOpen && <BackDrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLink />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
 
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
 
-      <nav>...</nav>
-    </MainHeader>
+        <nav className="main-navigation__header-nav">
+          <NavLink />
+        </nav>
+      </MainHeader>
+    </Fragment>
   );
 };
 
