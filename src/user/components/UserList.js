bceb9083bac3eb84a1fd88878d6241172bfc6eec
phamus <sphamus@gmail.com>
2020-02-15 19:11:59 +0100

    navigation

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,12 +1,16 @@
 import React from "react";
-import "./UserList.css";
+
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElement/Card";
+import "./UserList.css";
 
 const UserList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No User Found</h2>
+        <Card>
+          <h2>No User Found</h2>
+        </Card>
       </div>
     );
   }
