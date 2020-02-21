715224f497990ff6b6eac25f67b0602b61016f3f
phamus <sphamus@gmail.com>
2020-02-21 10:46:26 +0100

    authenication for displayed links

--- src/shared/components/Navigation/NavLink.js
+++ src/shared/components/Navigation/NavLink.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
 import "./NavLink.css";
 
+import { AuthContext } from "../../context/auth-context";
+
 const Navlink = props => {
+  const auth = useContext(AuthContext);
   return (
     <ul className="nav-links">
       <li>
@@ -11,15 +14,22 @@ const Navlink = props => {
           All Users
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places"> MY PLACES</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new"> NEW PLACE</NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">AUTHENTICATE</NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places"> MY PLACES</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new"> ADD PLACE</NavLink>
+        </li>
+      )}
+
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
