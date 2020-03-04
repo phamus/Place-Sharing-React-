94afa4490b8cca4b82b90556d1dd86f478f196b2
phamus <sphamus@gmail.com>
2020-03-04 15:23:02 +0100

    connection to login endpoint

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -38,11 +38,34 @@ const Auth = () => {
 
   const authSubmitHandler = async event => {
     event.preventDefault();
+    setIsLoading(true);
 
     if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:8000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+
+        const responseData = await response.json();
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        auth.login();
+      } catch (error) {
+        setIsLoading(false);
+        setError(error.message || "Something went wrong, please try again");
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch("http://localhost:8000/api/users/signUp", {
           method: "POST",
           headers: {
